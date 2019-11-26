430053c134110b59b099b46e07d6d5a3bb70710f
Matthew Gordon <matthew.scott.gordon@gmail.com>
2019-11-26 07:03:45 -0500

    Fix bug rendering images with dimensinos not multiples of tile size

--- src/main.rs
+++ src/main.rs
@@ -96,8 +96,8 @@ pub fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut i = 0;
     'running: loop {
         let tile_size = 128;
-        for tile_row in 0..(output_image.get_height() + 1) / tile_size {
-            for tile_column in 0..(output_image.get_width() + 1) / tile_size {
+        for tile_row in 0..1 + (output_image.get_height() + 1) / tile_size {
+            for tile_column in 0..1 + (output_image.get_width() + 1) / tile_size {
                 let row_start = tile_row * tile_size;
                 let row_end = min(tile_row * tile_size + tile_size, output_image.get_height());
                 let column_start = tile_column * tile_size;
